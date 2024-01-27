93a09857688f8634ab577e783fa411b95f7e05ba
miku77 <miku77@naver.com>
2024-01-27 21:17:13 +0900

    1111

--- docs/sw.js
+++ docs/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-1706354361';
+const cacheName = 'chirpy-1706357790';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
